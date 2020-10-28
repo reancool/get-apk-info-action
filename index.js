3b1eaa2236e0ba26856d4e160228f88d10389761
reancool <reancool@163.com>
2020-10-28 19:02:07 +0800

    Update index.js
    addb fromat buildDate

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 const core = require('@actions/core');
 const AppInfoParser = require('app-info-parser');
 
-async function main() {
-    try {
-        // inputs from action
-        const apkPath = core.getInput('apkPath');
-
-        console.log(apkPath);
-
-        const parser = new AppInfoParser(apkPath);// or xxx.ipa
-        parser.parse().then(result => {
-
-            core.setOutput("versionCode", result.versionCode);
-            core.setOutput("versionNum", result.versionName);
-            core.setOutput("applicationId", result.package);
-            core.setOutput("name", result.application.label);
-            core.setOutput("buildDate",  getDate());
-           
-            console.log('app info ----> ', result);
-
-        }).catch(err => {
-            console.log('err ----> ', err)
-        });
-    } catch (error) {
-        console.log('catch error ----> ', error);
-        core.setFailed(error.message);
-    }
-}
 function getDate(){
     var timezone = 8; 
     var offset_GMT = new Date().getTimezoneOffset();
@@ -46,6 +20,32 @@ function getDate(){
     second = second < 10 ? ('0' + second) : second;
     // return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
     return y + '_' + m + '_' + d+'_'+h+'-'+minute+'-'+second;
+}
+async function main() {
+    try {
+        // inputs from action
+        const apkPath = core.getInput('apkPath');
+        console.log(apkPath);
+        var date=getDate();
+        console.log('app build date ----> ', date);
+        const parser = new AppInfoParser(apkPath);// or xxx.ipa
+        parser.parse().then(result => {
+
+            core.setOutput("versionCode", result.versionCode);
+            core.setOutput("versionNum", result.versionName);
+            core.setOutput("applicationId", result.package);
+            core.setOutput("name", result.application.label);
+            core.setOutput("buildDate",  date);
+           
+            console.log('app info ----> ', result);
+
+        }).catch(err => {
+            console.log('err ----> ', err)
+        });
+    } catch (error) {
+        console.log('catch error ----> ', error);
+        core.setFailed(error.message);
+    }
 }
 
 main();
