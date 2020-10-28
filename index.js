6be05b079f0fa10832c27a80f707a13f7d1b512c
reancool <reancool@163.com>
2020-10-28 18:48:23 +0800

    Update index.js
    增加buildDate

--- index.js
+++ index.js
@@ -15,17 +15,37 @@ async function main() {
             core.setOutput("versionNum", result.versionName);
             core.setOutput("applicationId", result.package);
             core.setOutput("name", result.application.label);
+            core.setOutput("buildDate",  getDate());
+           
             console.log('app info ----> ', result);
 
         }).catch(err => {
             console.log('err ----> ', err)
         });
-
     } catch (error) {
         console.log('catch error ----> ', error);
         core.setFailed(error.message);
     }
 }
-
+function getDate(){
+    var timezone = 8; 
+    var offset_GMT = new Date().getTimezoneOffset();
+    var nowDate = new Date().getTime(); 
+    var targetDate = new Date(nowDate + offset_GMT * 60 * 1000 + timezone * 60 * 60 * 1000);
+    var date = new Date(targetDate);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    // return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
+    return y + '_' + m + '_' + d+'_'+h+'-'+minute+'-'+second;
+}
 
 main();
